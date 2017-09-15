4c5e566afdbee8a5d83fdefce19aad71c295a2bc
Tifozoso <mfranks@gmail.com>
2017-09-15 16:33:49 -0700

    Initial Commit

---
+++ Swapping Screens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swapping Screens
+//
+//  Created by Michael Franks on 9/15/17.
+//  Copyright © 2017 Popcorn Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
